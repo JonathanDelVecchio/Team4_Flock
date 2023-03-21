b87fc257b31362eee545c0202a537ab18015cca3
Nicholas Tan <nicholas.tan01@gmail.com>
2023-03-21 12:18:09 -0400

    added controller methods

--- untitled11/src/main/java/com/sg/flock/controller/Controller.java
+++ untitled11/src/main/java/com/sg/flock/controller/Controller.java
@@ -8,6 +8,31 @@ package com.sg.flock.controller;
  *
  * @author nicho
  */
+
+@RestController
+@RequestMapping("/api")
 public class Controller {
+    @Autowired
+    FlockDao dao;
     
+    @PostMapping("/post")
+    @ResponseStatus(HttpStatus.CREATED)
+    public int createPost() {
+        return dao.insertTweet();
+    }
+    
+    @PostMapping("/reply")
+    public int createReply() {
+        return dao.insertReply();
+    }
+    
+    @GetMapping("/posts")
+    public List<Tweet> getAllPosts() {
+        return dao.getAllTweets();
+    }
+    
+    @GetMapping("/post/{tweetId}")
+    public Reply<Reply> getAllReplies(@PathVariable("tweetId") int tweetId) {
+        return dao.getRepliesForTweetId(tweetId);
+    }
 }
