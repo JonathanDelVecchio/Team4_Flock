03e5f19ff6a5c8cb260cf4f046933fd2ffba4aa1
Andrew Wang <aw6461@gmail.com>
2023-03-22 13:37:48 -0400

    should connect to frontend now

--- untitled11/src/main/java/com/sg/flock/controller/Controller.java
+++ untitled11/src/main/java/com/sg/flock/controller/Controller.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author nicho
  */
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class Controller {
