da24ef3c3bccd5394cae5553611ab695614a5c26
Jonathan Del Vecchio <Jonathan@jonathandelvecchio.dev>
2023-03-24 11:40:22 -0700

    added search function

--- flockfrontend/src/app/search-tweets/search-tweets.component.ts
+++ flockfrontend/src/app/search-tweets/search-tweets.component.ts
@@ -1,29 +1,24 @@
-/*
-
 import { Component, OnInit } from '@angular/core';
 import { TweetService } from '../services/tweet.service';
 import { Tweet } from '../models/tweet';
 
 @Component({
- selector: 'app-search-tweets',
- templateUrl: './search-tweets.component.html',
- styleUrls: ['./search-tweets.component.scss']
+  selector: 'app-search-tweets',
+  templateUrl: './search-tweets.component.html',
+  styleUrls: ['./search-tweets.component.scss']
 })
 export class SearchTweetsComponent implements OnInit {
- searchQuery = '';
- tweets: Tweet[] = [];
+  searchQuery = '';
+  tweets: Tweet[] = [];
 
- constructor(private tweetService: TweetService) {
- }
+  constructor(private tweetService: TweetService) { }
 
- ngOnInit(): void {
- }
+  ngOnInit(): void {
+  }
+
+  searchTweets(): void {
+    this.tweetService.searchTweetsByUsername(this.searchQuery).subscribe((tweets) => {
+      this.tweets = tweets;
+    });
+  }
 }
- /*
- searchTweets(): void {
-   this.tweetService.searchTweetsByUsername(this.searchQuery).subscribe((tweets) => {
-     this.tweets = tweets;
-   });
- }
-}
-*/
